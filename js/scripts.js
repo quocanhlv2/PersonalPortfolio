2a55f1ed24270afde66f69fd6955a17fb6abff39
quocanhlv2 <quocanhlv2@users.noreply.github.com>
2021-07-19 17:15:50 +0700

    Updated Carousel
    Updated Version

--- js/scripts.js
+++ js/scripts.js
@@ -23,20 +23,38 @@ const sr = ScrollReveal({
 
 sr.reveal('#menu', {delay:50})
 
-/*sr.reveal('.transition', {delay: 100})*/
-sr.reveal('.portGallery', {delay: 100})
-
-
+sr.reveal('#portGallery1', {delay:100})
 
 sr.reveal('.biotext', {delay: 200})
 sr.reveal('.biotypo', {delay: 100})
-/*sr.reveal('.transition2', {delay: 100})*/
 
-
-/*sr.reveal('.transition3', {delay: 100})*/
 sr.reveal('.contentfigure', {delay: 100})
 sr.reveal('.contentcontact', {delay: 200})
 
+var slideIndex = 1;
+function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("portGallery");
+   var dots = document.getElementsByClassName("dot");
+   if (n > slides.length) {slideIndex = 1}
+   if (n < 1) {slideIndex = slides.length}
+    
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "flex";
+   dots[slideIndex-1].className += " active";
+ }
 
+ showSlides(slideIndex);
 
 
